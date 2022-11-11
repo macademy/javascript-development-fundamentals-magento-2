04884b90909e1dfeb1a57faae0b2b00b1f474584
Mark Shust <mark@shust.com>
2022-11-11 15:25:12 -0500

    Create a custom RequireJS config file

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+/*
+ * Magento looks for RequireJS configuration files within each module's
+ * view/frontend folder named requirejs-config.js.
+ * 
+ * This config object will be injected into require.config() when Magento
+ * loops through all of these files. Any specific configuration relating to
+ * RequireJS goes within this config object.
+ */
+var config = {
+
+};
