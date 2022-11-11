316219c81c599cb9dca9400dd5fd5ca42be0026d
Mark Shust <mark@shust.com>
2022-11-11 15:28:26 -0500

    Access other JavaScript components with RequireJS

--- view/frontend/web/js/fade-in-element.js
+++ view/frontend/web/js/fade-in-element.js
@@ -1,18 +1,10 @@
-// JavaScript Components use the "define" keyword rather than "require"
+// Architect your custom modules in ways that make them composable & reusable.
 define(['jquery'], function($) {
     'use strict'
 
-    /*
-     * To create reusable JavaScript Components, it's best practice to not define
-     * implementation details. The functionality of this component dynamically
-     * changes based on what parameters are passed to it.
-     */
+    // This means keep things generic, use arguments, ...
     return function (className, duration) {
-        /*
-         * Since parameter defaults only exist in ES6 and beyond, to make code
-         * with the highest level of compatibility, you can define defaults with
-         * the "||" (OR) symbol to define value fallbacks.
-         */
+        // ...and define defaults for unpassed arguments.
         $(className).hide().fadeIn(duration || 2000);
     };
 });
