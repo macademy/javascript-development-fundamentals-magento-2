57c89bd432f7b18ed56376142fb6b85aed31acf1
Mark Shust <mark@shust.com>
2022-11-11 15:33:35 -0500

    Declaratively initialize a JavaScript component with x-magento-init

--- view/frontend/web/js/vue-test.js
+++ view/frontend/web/js/vue-test.js
@@ -1,6 +1,8 @@
 /**
- * The properties defined within the attribute are now accessible within the callback. For example,
- * the "message" property is now accessible as config.message.
+ * If you use "*" to target all elements, the element property within the
+ * callback will be null because RequireJS won't know which specific element you
+ * wish to refeference. In this case, hardcode a selector or use an HTML class
+ * attribute rather than an ID selector within your JS initialization.
  */
 define(['vue'], function(Vue) {
     'use strict'
