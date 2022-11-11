b656db8fa68e5b27ec5646735495092376537bb1
Mark Shust <mark@shust.com>
2022-11-11 15:29:18 -0500

    Create RequireJS aliases with map and paths

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,11 +1,23 @@
 /*
- * Magento looks for RequireJS configuration files within each module's
- * view/frontend folder named requirejs-config.js.
- * 
- * This config object will be injected into require.config() when Magento
- * loops through all of these files. Any specific configuration relating to
- * RequireJS goes within this config object.
- */
-var config = {
+The "*" in the "map" prop makes the alias available to all JavaScript components.
+The format for aliases is: "alias": "<Vendor_Module>/js/filename"
 
+The area can either be "base", "frontend", or "adminhtml".
+
+<Vendor_Module>/js/filename
+maps to ->
+<Vendor>/<Module>/view/<area>/web/js/filename.js
+
+Paths can be used for non-AMD JavaScript modules & external files, just be sure
+to not include the ".js" extension as this is automatically appended.
+*/
+var config = {
+    "map": {
+        "*": {
+            "fadeInElement": "Macademy_JsFun/js/fade-in-element"
+        }
+    },
+    "paths": {
+        "vue": "https://cdn.jsdelivr.net/npm/vue/dist/vue"
+    }
 };
